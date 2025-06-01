039ca5cf74bff8b469f2cbdf76e70767b4782bcd
Yaguala <ricandrade4820.005@gmail.com>
2025-06-01 23:20:52 +0200

    test to try to solve the searchbox modele

--- pages/Sugestion.py
+++ pages/Sugestion.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.title("Choisi un filme")
 
 #Load data frame and show if needed:
-df_movies = pd.read_csv("projet_2\\films_final.csv")
+df_movies = pd.read_csv("films_final.csv")
 #st.write(df_movies)
 dummies = df_movies.genres.str.get_dummies(sep=',').drop(columns="Comedy")
 genres = dummies.columns.to_list()
