d1e50682519ee47848d52ab54c3e1d7dceed5e80
Yaguala <88831155+Yaguala@users.noreply.github.com>
2025-06-06 00:02:14 +0200

    Update Contacts.py

--- pages/Contacts.py
+++ pages/Contacts.py
@@ -1,7 +1,7 @@
 # Nos packages
 import streamlit as st
 from datetime import datetime
-from PIL import Image # il sert à ouvrir les images dans streamlit
+#from PIL import Image # il sert à ouvrir les images dans streamlit
 import pandas as pd
 import os # gérer des fichiers et des chemins dans mon ordi.  il sert de vérifier un fichier existant, créer des dossiers
 
@@ -11,8 +11,8 @@ import os # gérer des fichiers et des chemins dans mon ordi.  il sert de vérif
 left_co, cent_co,last_co = st.columns(3)
 with cent_co:
     # le chemin de l'image de notre logo
-    logo = Image.open(r"assets\image SAPEM.png") 
-    st.image(logo, width=150) # paramètre du logo
+    #logo = Image.open(r"assets\image SAPEM.png") 
+    #st.image(logo, width=150) # paramètre du logo
 #-------En-tête avec bouton à droite et Titre principal de l'application (affiché en haut de la page)
 
 # --- Initialisation de l'état ---
