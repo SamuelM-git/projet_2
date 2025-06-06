7e29c6bcc59c827976531e17aca518ead95dadf8
SamuelM-git <samuelmathi3@gmail.com>
2025-06-06 15:45:23 +0200

    Update film.py test maj

--- pages/film.py
+++ pages/film.py
@@ -1,4 +1,3 @@
-
 # Nos packages
 import streamlit as st
 import pandas as pd
@@ -39,7 +38,8 @@ if st.button("nm0000079"):
 
 st.divider()
 
-
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 # Le temps
 # https://docs.streamlit.io/develop/api-reference/widgets/st.date_input
@@ -135,50 +135,3 @@ if selected is not None:
 
 
 st.write(' 🦁 ')
-
-# Load data frame and show if needed:
-df_movies = pd.read_csv("data/films_final.csv")
-df_movies
-
-'''#---------------- Load page --------------------------------
-df_movies = pd.read_csv("data/films_final.csv")
-
-# -------- Get the film ID from the query params -------------
-film_id = st.query_params.get("name", [None])[0]
-
-# -------- Set session state if the film_id is found ----------
-if film_id:
-    st.session_state["selected_film"] = film_id
-
-# -------- Lookup the film in the DataFrame ----------
-tconst = st.session_state.get("selected_film")
-
-if tconst:
-    df_selected = df_movies[df_movies["tconst"].str.contains(tconst)]
-
-    if not df_selected.empty:
-        # Show the correct title
-        st.header(df_selected["title"].iloc[0], divider="green")
-
-        # Optional: show more info
-        st.write(df_selected.iloc[0])
-    else:
-        st.warning("Film not found in the dataset.")
-else:
-    st.warning("No film ID provided in query parameters.")
-'''
-
-#--------------- Selection film from carrousell-----------------
-name = st.query_params.get("name", None)
-
-if name:
-    st.session_state["selected_intervenant"] = name
-
-if "selected_intervenant" not in st.session_state:
-    st.session_state["selected_intervenant"] = "nm0000091" # A changer pour une film base
-
-tconst = st.session_state.selected_intervenant    
-df_inter = df_movies[df_movies['tconst'] == tconst]
-
-
-st.header(df_inter['title'].iloc[0], divider="green")
