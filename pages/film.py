765565e49369ee4a1a3ff98deb6d7fdccb676d65
SamuelM-git <samuelmathi3@gmail.com>
2025-06-08 16:15:43 +0200

    Update film.py maj test 1

--- pages/film.py
+++ pages/film.py
@@ -46,6 +46,20 @@ left.image(df_inter.poster_path.iloc[0], caption="l'affiche")
 middle.write(resume_film)
 right.write(infos_film)
 st.write("-------")
+
+#Def 2 col in intervenant
+col1, col2 = st.columns(2)
+# Col1 intervenant
+col1.image(df_inter['profile_photo'].iloc[0])
+#Col2 intervenant
+#Preparation text to descrive intervenant
+resume_film_2 = "Résumé : \n"+df_inter.overview.iloc[0]
+
+col2.header("Résumé :", divider="green")
+col2.markdown("""<style>.big-font {font-size:20px !important;}</style>""", unsafe_allow_html=True)
+col2.markdown(f'<p class="big-font">{resume_film_2}</p>', unsafe_allow_html=True)
+col2.markdown(f'<p class="big-font">{resume_film}</p>', unsafe_allow_html=True)
+
 """
  for i in range(100):
      if df_intervenant.knownForTitles.iloc[i].isin(df_inter.popularity.iloc[0]) :
