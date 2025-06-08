42efda151ae8aa7fdfe4a24c1d0a16f739d22888
SamuelM-git <samuelmathi3@gmail.com>
2025-06-08 17:08:07 +0200

    Update film.py ok5

--- pages/film.py
+++ pages/film.py
@@ -20,7 +20,6 @@ df_intervenant_1950 = pd.read_csv("data/intervenant_tmdb_find_by_ID_1950_r5.csv"
 
 #--------------- Selection film from carrousell-----------------
 name = st.query_params.get("name", None)
-st.write(name)
 
 if name:
     st.session_state["selected_intervenant"] = name
@@ -35,14 +34,13 @@ if st.session_state.selected_intervenant :
     titre_du_film = df_inter['title'].iloc[0]
 
 st.header("Titre du film : "+titre_du_film, divider="green")
+# On teste si il y a une bande annonce. Si il y en a pas, on affiche le message d'absence, sinon on affiche la video
 if df_inter.bande_annonce.iloc[0] == "pas disponible":
     st.write("Désolè, pas de bande annonce disponible")
 else:
     st.video(df_inter.bande_annonce.iloc[0])
 st.write("-------")
 # Le bloc de 3 parties de présentation : l'affiche, le resume et les autres infos
-#resume_film = df_inter.overview.iloc[0]
-#infos_film = "Populaité : "+str(df_inter.popularity.iloc[0])+"\nAnnée de sortie : "+str(df_inter.startYear.iloc[0])+"\nGenre : "+df_inter.genres.iloc[0]+"\nScore : "+str(df_inter.averageRating.iloc[0])+" sur "+str(df_inter.numVotes.iloc[0])+" votant(s)."
 left, middle, right = st.columns(3, border=True)
 left.image(df_inter.poster_path.iloc[0], caption="l'affiche")
 middle.write("Résumé :")
@@ -53,16 +51,11 @@ right.write("Genre : "+df_inter.genres.iloc[0])
 right.write("Score : "+str(df_inter.averageRating.iloc[0])+" sur "+str(df_inter.numVotes.iloc[0])+" votant(s)")
 st.write("-------")
 
-
-"""
  for i in range(100):
      if df_intervenant.knownForTitles.iloc[i].isin(df_inter.popularity.iloc[0]) :
-         left, middle, right = st.columns(3, border=True)
-         left.image(df_intervenant.poster_path.iloc[i], caption=df_intervenant.primaryName.iloc[0)
-         middle.image(df_intervenant.poster_path.iloc[i], caption="backdrop_path")
-         right.write("Test en cours")
+         st.write("trouve en "+i)
 st.write("-------")
-"""
+
 if st.button("nm0000003"):
     st.session_state.selected_intervenant = "nm0000003"
     st.switch_page("pages/intervenant.py")
