6d5c980c455bc617707a1978ebf2807356b243f9
SamuelM-git <samuelmathi3@gmail.com>
2025-06-08 22:30:57 +0200

    Update film.py uh

--- pages/film.py
+++ pages/film.py
@@ -21,9 +21,9 @@ df_intervenant_1950 = pd.read_csv("data/intervenant_tmdb_find_by_ID_1950_r5.csv"
 #--------------- Selection film from carrousell-----------------
 name = st.query_params.get("name", None)
 
-#if name:
-#    st.session_state["selected_intervenant"] = name
-#    titre_du_film = "Film par defaut dans if"
+if name:
+    st.session_state["selected_intervenant"] = name
+    titre_du_film = "Film par defaut dans if"
 
 #if "selected_intervenant" not in st.session_state:
 #    st.session_state["selected_intervenant"] = "nm0000091" # A changer pour une film base
@@ -32,9 +32,8 @@ if st.session_state.selected_intervenant :
     tconst = st.session_state.selected_intervenant    
     df_inter = df_movies[df_movies['tconst'] == tconst]
     titre_du_film = df_inter['title'].iloc[0]
-# le film affiché par defaut "Adieu les cons"
-else :
-    tconst = "tt10196398"    
+else :        
+    tconst = "tt10196398"    # le film affiché par defaut "Adieu les cons"
     df_inter = df_movies[df_movies['tconst'] == tconst]
     titre_du_film = df_inter['title'].iloc[0]
     
